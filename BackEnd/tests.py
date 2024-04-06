76d975773fa1cad96397a259b15c8c96c3f991e0
Lucas Meira <lucasrenaa1996@gmail.com>
2024-04-06 21:44:57 +0000

    criando os testes e garantindo que somente o administrador possa criar tickets/deletar e editar

--- BackEnd/tests.py
+++ BackEnd/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .views import TicketViewSet
+from .models import Ticket
 
-# Create your tests here.
+class TicketViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create(username='admin', is_staff=True)
+        self.normal_user = User.objects.create(username='user', is_staff=False)
+        self.ticket = Ticket.objects.create(name='T001', setor='Financeiro')
+
+    def tearDown(self):
+        Ticket.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_admin_can_post(self):
+        factory = APIRequestFactory()
+        view = TicketViewSet.as_view({'post': 'create'})
+        url = reverse('ticket-list')
+        data = {'name': 'T002', 'setor': 'Tecnologia'}
+        request = factory.post(url, data)
+        force_authenticate(request, user=self.admin_user)
+        response = view(request)
+        self.assertEqual(response.status_code, 201)
+
+    def test_normal_user_cannot_post(self):
+        factory = APIRequestFactory()
+        view = TicketViewSet.as_view({'post': 'create'})
+        url = reverse('ticket-list')
+        data = {'name': 'T002', 'setor': 'Tecnologia'}
+        request = factory.post(url, data)
+        force_authenticate(request, user=self.normal_user)
+        response = view(request)
+        self.assertEqual(response.status_code, 403)
+    def test_no_user_user_cannot_post(self):
+        factory = APIRequestFactory()
+        view = TicketViewSet.as_view({'post': 'create'})
+        url = reverse('ticket-list')
+        data = {'name': 'T002', 'setor': 'Tecnologia'}
+        request = factory.post(url, data)
+        response = view(request)
+        self.assertEqual(response.status_code, 401)
+
+    def test_admin_can_get(self):
+        factory = APIRequestFactory()
+        view = TicketViewSet.as_view({'get': 'list'})  # Usar 'list' em vez de 'create' para obter uma lista de recursos
+        url = reverse('ticket-list')
+        request = factory.get(url)
+        force_authenticate(request, user=self.admin_user)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_normal_user_cannot_get(self):
+        factory = APIRequestFactory()
+        view = TicketViewSet.as_view({'get': 'list'})  # Usar 'list' em vez de 'create' para obter uma lista de recursos
+        url = reverse('ticket-list')
+        request = factory.get(url)
+        force_authenticate(request, user=self.normal_user)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_no_user_cannot_get(self):
+        factory = APIRequestFactory()
+        view = TicketViewSet.as_view({'get': 'list'})  # Usar 'list' em vez de 'create' para obter uma lista de recursos
+        url = reverse('ticket-list')
+        request = factory.get(url)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+
+    def run(self, result=None):
+        super().run(result)
+        if result.wasSuccessful():
+            print("\nO teste passou! ")
+        else:
+            print("\nAlguns testes falharam. Verifique a saída abaixo para mais detalhes.")
--- BackEnd/views.py
+++ BackEnd/views.py
@@ -7,9 +7,19 @@ from rest_framework import permissions
 
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import  DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
+class AdminOnlyPermission(BasePermission):
+    """
+    Permite apenas que os administradores realizem operações de criação, atualização e exclusão.
+    """
+    def has_permission(self, request, view):
+        # Apenas permitir se o usuário for um administrador e estiver autenticado
+        return request.user and request.user.is_authenticated and request.user.is_staff
 
 class UserPermission(permissions.BasePermission):
 
@@ -19,16 +29,25 @@ class UserPermission(permissions.BasePermission):
 
 
 class TicketViewSet(viewsets.ModelViewSet):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly, UserPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-    http_method_names = ['get', 'post', 'put', 'path','delete']
+    http_method_names = ['get', 'post', 'put', 'patch','delete']
 
 
 class TicketAtualizacaoViewSet(viewsets.ModelViewSet):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly, UserPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
     queryset = TicketAtualizacao.objects.all()
     serializer_class = TicketAtualizacaoSerializer
-    http_method_names = ['get', 'post', 'put', 'path','delete']
+    http_method_names = ['get', 'post', 'put', 'patch','delete']
