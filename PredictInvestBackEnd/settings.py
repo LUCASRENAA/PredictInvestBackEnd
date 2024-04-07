d5bb491a9ce3a05d84ac7db67961dd9801740f85
Lucas Meira <lucasrenaa1996@gmail.com>
2024-04-07 18:32:34 -0300

    adicionando a opção do cors

--- PredictInvestBackEnd/settings.py
+++ PredictInvestBackEnd/settings.py
@@ -80,8 +80,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'PredictInvestBackEnd.wsgi.application'
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = True # CORS_ALLOWED_ORIGIN = 'http://localhost:3000/'
-
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
